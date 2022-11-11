5d98f94c955af7e5dc83e2f9c106f698896062fe
Jefgrim <jefgrimalvar@gmail.com>
2022-11-11 20:34:13 +0800

    Testing full screen and landscape for mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 // get the mainContainer
 let mainContainer = document.querySelector(".mainContainer");
 let restartBtn = document.querySelector(".restartBtn");
+
 // if screen is width is less than 768px do this
 if (window.innerWidth < 768) {
+  restartBtn.addEventListener("click", fullscreen);
+}
+
+// event listener for changing window size
+window.addEventListener("resize", reportWindowWidth);
+
+function reportWindowWidth() {
+  if (window.innerWidth < 768) {
     restartBtn.addEventListener("click", fullscreen);
-    function fullscreen(){
-        mainContainer.requestFullscreen();   
-    }
+  } else {
+    restartBtn.removeEventListener("click", fullscreen);
+  }
+}
+
+function fullscreen() {
+  mainContainer.requestFullscreen();
+  screen.orientation.lock("landscape-primary");
 }

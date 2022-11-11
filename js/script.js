b4b4276feeacfb0f82123cc66f0c9439da40e675
Jefgrim <jefgrimalvar@gmail.com>
2022-11-11 22:52:24 +0800

    quit btn in smallscreen prompt and some logic

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 let mainContainer = document.querySelector(".mainContainer");
 let restartBtn = document.querySelector(".restartBtn");
 let fullScreenBtn = document.querySelector("#fullScreenBtn");
+let quitBtn = document.querySelector("#quitBtn");
 let smallScreenPrompt = document.querySelector("#smallScreenPrompt");
+let smallScreenPromptTxt = document.querySelector("#smallScreenPromptTxt");
 
 // if screen is width is less than 768px do this
 if (window.innerWidth < 768) {
@@ -27,7 +29,20 @@ function reportWindowWidth() {
   }
 }
 
+// toggle fullscreen and landscape mode especially for mobile
 function fullscreen() {
   mainContainer.requestFullscreen();
   screen.orientation.lock("landscape-primary");
+  if (window.innerWidth < 768) {
+    document.exitFullscreen();
+    smallScreenPromptTxt.textContent =
+      "Your Screen is too small for this game, Sorry.";
+    fullScreenBtn.style.display = "none";
+    quitBtn.style.display = "block";
+    quitBtn.addEventListener("click", quitGame);
+
+    function quitGame() {
+      window.close();
+    }
+  }
 }

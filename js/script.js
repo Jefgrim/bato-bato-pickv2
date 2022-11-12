b1e7cf51892e393e9f1f74b3a1c66968405825a1
Jefgrim <jefgrimalvar@gmail.com>
2022-11-12 15:08:22 +0800

    improvements in design, added exit button

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
-// get the mainContainer
+// get the mainContainer element
 let mainContainer = document.querySelector(".mainContainer");
+
+// get button elements
 let restartBtn = document.querySelector(".restartBtn");
 let fullScreenBtn = document.querySelector("#fullScreenBtn");
 let quitBtn = document.querySelector("#quitBtn");
+let rockMoveBtn = document.querySelector("#rockMoveBtn");
+let paperMoveBtn = document.querySelector("#paperMoveBtn");
+let scissorMoveBtn = document.querySelector("#scissorMoveBtn");
+
+// get recemt move container
+let recentMovesImgContainer = document.querySelector(
+  "#recentMovesImgContainer"
+);
+
+// get score display container
+let playerScoreDisplay = document.querySelector("#playerScoreDisplay");
+let botScoreDisplay = document.querySelector("#botScoreDisplay");
+
+// get the small screen prompt element
 let smallScreenPrompt = document.querySelector("#smallScreenPrompt");
 let smallScreenPromptTxt = document.querySelector("#smallScreenPromptTxt");
 
-// if screen is width is less than 768px do this
+// if screen width is less than 768px do this
 if (window.innerWidth < 630) {
   fullScreenBtn.addEventListener("click", fullscreen);
 } else {
@@ -46,3 +62,5 @@ function fullscreen() {
     }
   }
 }
+
+// game code
